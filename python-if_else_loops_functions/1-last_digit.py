0c2ba31788ced2b19f03f452a9c4223febb1bde9
bcyuzuzo <b.cyuzuzo@alustudent.com>
2024-10-22 00:04:26 -0700

    fix typo

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ if last_digit > 5:
 elif last_digit == 0:
     print(f'Last digit of {number} is {last_digit} and is 0')
 elif last_digit < 6:
-    message = (f'Last digit of {} is {} and is less than 6 and not 0')
+    message = 'Last digit of {} is {} and is less than 6 and not 0'
     print(message.format(number, last_digit))
 else:
     ('I have no idea!')
