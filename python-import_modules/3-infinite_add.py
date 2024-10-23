5ee99d3a0970886460870751936d0c8545819ba6
bcyuzuzo <b.cyuzuzo@alustudent.com>
2024-10-23 03:15:33 -0700

    remove unnecessary checks

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,12 +3,10 @@ from sys import argv
 
 if __name__ == "__main__":
     result = 0
-    if len(argv) <= 1:
-        print(0)
-    elif len(argv) > 1:
-        for i in range(len(argv)):
-            if i == 0:
-                continue
-            else:
-                result += int(argv[i])
+    for i in range(len(argv)):
+        if i <= 0:
+            continue
+        else:
+            result += int(argv[i])
+
 print('{}'.format(result))
