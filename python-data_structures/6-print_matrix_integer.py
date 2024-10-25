19f4a794573c02f68b354ebbc164ff070463a082
bcyuzuzo <b.cyuzuzo@alustudent.com>
2024-10-25 09:14:58 -0700

    fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
-            print(row[i], end=" ")
+            print('{}'.format(row[i], end=" "))
         print()
