796551a5eeb852098c59545cb2865cecaa37815a
bcyuzuzo <b.cyuzuzo@alustudent.com>
2024-10-25 08:00:14 -0700

    add file 5

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    result = ''
+
+    for i in range(len(my_string)):
+        if my_string[i] != 'C' and my_string[i] != 'c':
+            result += my_string[i]
+
+    return result
