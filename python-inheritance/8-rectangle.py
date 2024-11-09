db9ce9ba2fbe898d7f24b88d8aafbb89c4470151
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-09 20:37:22 +0200

    add file 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''create a base geometry class'''
+
+
+class BaseGeometry:
+    '''raise an exception and validate value'''
+
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f'{name} must be an integer')
+        if value <= 0:
+            raise ValueError(f'{name} must be greater than 0')
+
+class Rectangle(BaseGeometry):
+    """Rectangle class inherits from BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Initialize rectangle with width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
