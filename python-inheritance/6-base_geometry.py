bdbd78a935b2dfb0a252efa85593761ff48c4ed4
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-09 11:42:25 +0200

    add file 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''create an base geometry class'''
+
+
+class BaseGeometry:
+    '''raise an exception'''
+
+    def area(self):
+        raise Exception('area() is not implemented')
