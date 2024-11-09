31e93a5d3eb087c0556d16d10361e671ef2b914e
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-09 20:44:53 +0200

    add file 8 short

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,27 +1,14 @@
 #!/usr/bin/python3
-'''create a base geometry class'''
-
-
-class BaseGeometry:
-'''raise an exception and validate value'''
-
-    def area(self):
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f'{name} must be an integer')
-        if value <= 0:
-            raise ValueError(f'{name} must be greater than 0')
+"""Module for Rectangle class."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-"""Rectangle class inherits from BaseGeometry."""
+    """Rectangle class that inherits from BaseGeometry."""
 
-    def init(self, width, height):
-"""Initialize rectangle with width and height."""
-        super().init()
-        self.integer_validator('width', width)
-        self.width = width
-        self.integer_validator('height', height)
-        self.height = height'
+    def __init__(self, width, height):
+        """Initialize rectangle with width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
