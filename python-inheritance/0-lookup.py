cfd15560c499ce7dd5087cdafcd27547b89dfd39
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-09 08:29:08 +0200

    add space

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' function for lookup method '''
 
+
 def lookup(obj):
-  ''' Return a list of attributes in the object '''
-  return dir(obj)
+    ''' Return a list of attributes in the object '''
+    return dir(obj)
