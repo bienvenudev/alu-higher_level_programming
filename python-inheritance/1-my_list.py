b63265f085b08150416bd7e5947a190983ec2e3a
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-09 08:59:46 +0200

    add file 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''A class to return list sorted in ascending order'''
+
+
+class MyList(list):
+    '''a public method to sort list'''
+    def print_sorted(self):
+        print(list.sort(self))
