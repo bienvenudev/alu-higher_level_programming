6c06e4b7d782fbb43c823991b913e054b7a38c39
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-09 11:17:47 +0200

    add file 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Function to check if object is the same class'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''Check if obj is an instance of the class'''
+    return isinstance(obj, a_class)
