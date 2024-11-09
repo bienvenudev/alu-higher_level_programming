d20357a0dda635a8663656c1babc7e72bf43b8f2
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-09 11:32:25 +0200

    use issubclass

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,4 +5,4 @@
 def inherits_from(obj, a_class):
     '''Check if obj is an instance of a class that inherited
     from the specified class'''
-    return type(obj) is not a_class
+    return issubclass(type(obj), a_class)
