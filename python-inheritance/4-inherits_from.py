bf03ffa5502d04f70b4f6da4659a470e0b58e2bc
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-09 11:23:33 +0200

    reduce characters

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,5 +3,6 @@
 
 
 def inherits_from(obj, a_class):
-    '''Check if obj is an instance of a class that inherited (directly or indirectly) from the specified class'''
+    '''Check if obj is an instance of a class that inherited 
+    from the specified class'''
     return type(obj) is not a_class
