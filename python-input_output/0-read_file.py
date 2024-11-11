03f8ed04aab6a9439e00520b85e2563c66e35659
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-11 21:17:23 +0200

    fix file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-    '''read a text file (UTF8) and print it to stdout'''
-    with open(f'{filename}', encoding='utf-8') as file:
-        print(file.read())
+    """Read a text file (UTF8) and print it to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
