ca7057f1faeb37d79e585d13bf0e3a5e4b595564
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-11 21:19:41 +0200

    add end of file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """Read a text file (UTF8) and print it to stdout."""
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end='')
