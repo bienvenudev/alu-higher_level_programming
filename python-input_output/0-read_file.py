471880ed29c1f44b2449e278f828b12e363c4d23
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-11 21:14:53 +0200

    add indentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-  '''read a text file (UTF8) and print it to stdout'''
-  with open(f'{filename}', encoding='utf-8') as file:
-    print(file.read())
+    '''read a text file (UTF8) and print it to stdout'''
+    with open(f'{filename}', encoding='utf-8') as file:
+        print(file.read())
