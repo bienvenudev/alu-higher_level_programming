3f20880d9fd16a6c38a58b4d281ba3ffc15f34dd
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-11 21:45:30 +0200

    add a blank line

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """a function that appends a string at the end of a text file"""
 
+
 def append_write(filename="", text=""):
     """returns the number of characters added:"""
     with open(filename, mode='a', encoding='utf-8') as file:
