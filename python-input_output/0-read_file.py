12ad0b0fd66bf4fadcbddfe6d79cb74c60bb4d39
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-11 21:11:44 +0200

    add documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+'''a function that reads a text file (UTF8) and prints it to stdout'''
 
 
 def read_file(filename=""):
+  '''read a text file (UTF8) and print it to stdout'''
   with open(f'{filename}', encoding='utf-8') as file:
     print(file.read())
