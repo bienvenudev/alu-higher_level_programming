91fc236504bcc56f4905501555fdefa8d713aa3c
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-12 19:38:26 +0200

    use r for mode and return

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """function for creating an object from json string"""
-    with open(filename, 'w', encoding='utf-8') as file:
-        json.load(file)
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
