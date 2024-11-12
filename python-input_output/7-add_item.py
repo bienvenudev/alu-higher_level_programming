478c124979f30272634210cd2969ed3607c86b42
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-12 20:24:51 +0200

    initial commit

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,25 @@
+import json
+import sys 
+
+args = sys.argv[1:]
+
+def save_to_json_file(my_obj, filename):
+    """function for writing an object to a text file"""
+    try:
+        with open(filename, 'r', encoding='utf-8') as f:
+            existing_data = json.load(f)
+    except FileNotFoundError:
+        existing_data = []
+
+    existing_data.extend(my_obj)
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(existing_data, f)
+
+
+def load_from_json_file(filename):
+    """function for creating an object from json string"""
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+save_to_json_file(args, 'add_item.json')
