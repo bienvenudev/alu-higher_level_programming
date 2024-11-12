5dfb834278beac7bbc83103e3dbf8c157ef1e7b5
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-12 20:42:03 +0200

    add file 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module for student class."""
+
+
+class Student:
+    """A class that defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve the dictionary representation of student."""
+        if attrs is None:
+            return self.__dict__
+        return {k: v for k, v in self.__dict__.items() if k in attrs}
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the student object"""
+        for key, value in json.items():
+            setattr(self, key, value)
