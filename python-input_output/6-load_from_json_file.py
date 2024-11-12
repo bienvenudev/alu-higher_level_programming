e72623919451e507f4dc5004b794dfb17d910a45
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-12 19:35:03 +0200

    ini

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for creating an obj from JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """function for creating an object from json string"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.load(file)
