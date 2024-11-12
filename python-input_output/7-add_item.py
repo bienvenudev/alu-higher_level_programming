d61bcc5a3c8b9408690ca39c4f81bae5c6eb3bc4
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-12 20:29:57 +0200

    import automatically and use path to check

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
 """Module for creating an obj from JSON file"""
-import json
 import sys
+from os import path
 
 
-args = sys.argv[1:]
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+filename = "add_item.json"
+if path.exists(filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
 
-def save_to_json_file(my_obj, filename):
-    """function for writing an object to a text file"""
-    try:
-        with open(filename, 'r', encoding='utf-8') as f:
-            existing_data = json.load(f)
-    except FileNotFoundError:
-        existing_data = []
-
-    existing_data.extend(my_obj)
-
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(existing_data, f)
-
-
-def load_from_json_file(filename):
-    """function for creating an object from json string"""
-    with open(filename, 'r', encoding='utf-8') as file:
-        return json.load(file)
-
-save_to_json_file(args, 'add_item.json')
+my_list.extend(sys.argv[1:])
+save_to_json_file(my_list, filename)
