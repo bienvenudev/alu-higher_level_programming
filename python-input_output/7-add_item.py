3bf7fe42ab50b8dff43782a33fa2fc8abe0aa4c4
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-12 20:48:12 +0200

    update file 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
 """Module for creating an obj from JSON file"""
 import sys
-from os import path
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+from os.path import exists
+from json import dump, load
 
-filename = "add_item.json"
-if path.exists(filename):
+def save_to_json_file(obj, filename):
+    """Write a Python object to a JSON file."""
+    with open(filename, 'w', encoding='utf-8') as file:
+        dump(obj, file)
+
+
+def load_from_json_file(filename):
+    """Load a Python object from a JSON file."""
+    with open(filename, 'r', encoding='utf-8') as file:
+        return load(file)
+
+
+filename = 'add_item.json'
+
+if exists(filename):
     my_list = load_from_json_file(filename)
 else:
     my_list = []
