132e2d57dad6e6db5338fe11406903bd70b7dda4
bcyuzuzo <b.cyuzuzo@alustudent.com>
2024-11-19 07:14:44 -0800

    add body size

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument and retrieves the size of the response body
-curl -so /dev/null "$1" -w '%{size_download}\n'
+# Takes in a URL, sends a request, and displays the size of the body
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
