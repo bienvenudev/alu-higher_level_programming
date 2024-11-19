aedbece8bc6970ca3058e70044f8059edede965a
Bienvenu <b.cyuzuzo@alustudent.com>
2024-11-19 15:43:11 +0000

    add body size

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a request, and displays the size of the body
-curl -so /dev/null "$1" -w '%{size_download}\n'
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
