ceda287c744a9145094ed1c7c7f1092c70071273
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-19 17:26:46 +0200

    update file

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request, and displays the size of the body
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+curl -so /dev/null "$1" -w '%{size_download}\n'
