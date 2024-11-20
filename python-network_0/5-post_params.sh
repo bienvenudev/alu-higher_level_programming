322c7748631d8205843ba62fd71734dd02e04c46
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-20 16:49:35 +0200

    update file 5

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-School-User-Id: 98" "$1"
+# Bash scripts that sends a POST request to a given URL.
+curl -s -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
