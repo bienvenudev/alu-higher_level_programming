01fbf6e6ec009516c8d9b2ee7c26fcd724963518
Bienvenu Cyuzuzo <Bienvenu>
2024-11-21 17:17:43 +0200

    use star wars

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+"""Sends a search request for a given string to the Star Wars API.
 
-Usage: ./8-json_api.py <letter>
-  - The letter is sent as the value of the variable `q`.
-  - If no letter is provided, sends `q=""`.
+Usage: ./9-starwars.py <search string>
+  - The search request is sent to the Star Wars API search people endpoint.
 """
 import sys
-import requests
+from requests import get, auth
 
 
 if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": letter}
-
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
-    try:
-        response = r.json()
-        if response == {}:
-            print("No result")
-        else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
-    except ValueError:
-        print("Not a valid JSON")
+    url = 'https://api.github.com/user'
+    user = sys.argv[1]
+    password = sys.argv[2]
+    r = get(url, auth=auth.HTTPBasicAuth(user, password))
+    print(r.json().get('id'))
