032a257ef1b055f9ba35621567e7d6d8e5d25027
Bienvenu Cyuzuzo <Bienvenu>
2024-11-25 17:26:19 +0200

    use if statement

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """Fetch a url"""
 
+
 import requests
 
-
-if __name__ == "__main__":
-    response = requests.get("https://alu-intranet.hbtn.io/status")
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
