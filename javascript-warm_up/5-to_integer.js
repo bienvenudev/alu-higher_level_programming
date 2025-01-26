f7e903ef619b116dd85958736ed4645bc5d3dfb9
bienvenudev <cbienvenu007@gmail.com>
2025-01-26 13:58:01 +0200

    add 4 and 5th file

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+const arg1 = Math.floor(Number(argv[2]));
+
+if (arg1) {
+  console.log(arg1);
+} else {
+  console.log('Not a number');
+}
