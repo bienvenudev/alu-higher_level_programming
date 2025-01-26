db141ac0027a501098aa1b6533425ef36556929a
bienvenudev <cbienvenu007@gmail.com>
2025-01-26 13:59:25 +0200

    fix output message

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -5,7 +5,7 @@ const { argv } = require('node:process');
 const arg1 = Math.floor(Number(argv[2]));
 
 if (arg1) {
-  console.log(arg1);
+  console.log('My number: ' + arg1);
 } else {
   console.log('Not a number');
 }
