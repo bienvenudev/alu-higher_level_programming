796a22fc972c6731e9a805cc150b07cc5d8ce981
bienvenudev <cbienvenu007@gmail.com>
2025-02-06 11:38:20 +0200

    correct output

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
-const Square1 = require('./5-square');
+const Sq = require("./5-square");
 
-module.exports = class Square extends Square1 {
-  constructor (size) {
-    super(size, size);
-    this.size = size;
-  }
-
-  charPrint (c) {
-    for (let i = 0; i < this.size; i++) {
-      c === undefined ? console.log('X'.repeat(this.size)) : console.log(c.repeat(this.width));
+class Square extends Sq {
+  charPrint(c) {
+    if (c) {
+      console.log(`${c.repeat(this.width)}\n`.repeat(this.height).trimEnd());
+    } else {
+      super.print();
     }
   }
-};
+}
+module.exports = Square;
