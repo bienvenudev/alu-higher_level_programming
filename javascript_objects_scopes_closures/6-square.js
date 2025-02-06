da21aa9f7f516e6f08dc8e271cf7845a08b55a66
bienvenudev <cbienvenu007@gmail.com>
2025-02-06 11:39:59 +0200

    add space before function

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -2,7 +2,7 @@
 const Sq = require('./5-square');
 
 class Square extends Sq {
-  charPrint(c) {
+  charPrint (c) {
     if (c) {
       console.log(`${c.repeat(this.width)}\n`.repeat(this.height).trimEnd());
     } else {
